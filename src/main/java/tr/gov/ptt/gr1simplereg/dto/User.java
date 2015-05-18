efec7b1040001dcdc39223c27f2e7a8a0b7875d1
Administrator <Administrator@10.10.10.59>
2015-05-18 11:16:22 +0300

    11:15

---
+++ src/main/java/tr/gov/ptt/gr1simplereg/dto/User.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr.gov.ptt.gr1simplereg.dto;
+
+/**
+ *
+ * @author Administrator
+ */
+public class User {
+    
+    
+   private Integer id;
+   private String username;
+   private String password;
+   private String firstname;
+   private String lastname;
+   private String email;
+   private String phone;
+   private String age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+   
+   
+    
+}
---
+++ src/main/java/tr/gov/ptt/gr1simplereg/mb/UserController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr.gov.ptt.gr1simplereg.mb;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import tr.gov.ptt.gr1simplereg.dto.User;
+
+/**
+ *
+ * @author Administrator
+ */
+public class UserController {
+    
+    private User registrationUser;
+
+    public UserController(User registrationUser) {
+        this.registrationUser = new User();
+    }
+    
+    
+
+    public User getRegistrationUser() {
+        return registrationUser;
+    }
+
+    public void setRegistrationUser(User registrationUser) {
+        this.registrationUser = registrationUser;
+    }
+    
+    public String register(){
+    
+       System.out.println("Register User :"+this.registrationUser);
+       String msg = "User Registered Successfully";
+       FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, msg, msg));
+       
+       
+       FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+       return "registration.jsf?faces-redirect=true"; //url de yazması için
+    }
+    
+    
+    
+}
